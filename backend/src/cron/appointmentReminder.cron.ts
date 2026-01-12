c6386dabc1263407d5b2e208a4a142a60853a57f
kajolsinghme <kajolsingh985@gmail.com>
2026-01-13 01:42:09 +0530

    feat(appointments): implement cron job for appointment reminders via email

--- backend/src/cron/appointmentReminder.cron.ts
+++ backend/src/cron/appointmentReminder.cron.ts
@@ -1,7 +1,36 @@
-import cron from 'node-cron'
+import cron from 'node-cron';
+import { AppointmentModel } from '../models/appointment.model.js';
+import { AppointmentStatus } from '../constants/appointment-status.js';
+import { sendMail } from '../services/email.service.js';
+import { UserModel } from '../models/user.model.js';
+import { appointmentReminderTemplate } from '../templates/appointmentReminder.template.js';
 
 export const startAppointmentReminderCron = () => {
-    cron.schedule('* * * * * *', () => {
-        console.log('running a task every minute')
-    })
-}
+  cron.schedule('* * * * * ', async () => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+
+    const appointments = await AppointmentModel.find({
+      scheduledAt: {
+        $gte: new Date(tenMinutesLater.getTime() - 60 * 1000),
+        $lte: tenMinutesLater,
+      },
+      status: AppointmentStatus.BOOKED,
+    });
+
+    for (const appointment of appointments) {
+      const patientId = appointment.patientId;
+      const doctorId = appointment.doctorId;
+      const patientData = await UserModel.findById(patientId);
+      const doctorData = await UserModel.findById(doctorId);
+      await sendMail({
+        to: patientData!.email!,
+        subject: '⏰ Appointment Reminder',
+        html: appointmentReminderTemplate(
+          patientData!.name,
+          doctorData!.name,
+          appointment.scheduledAt.toLocaleString()
+        ),
+      });
+    }
+  });
+};
